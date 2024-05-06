f381a2a8d95ddecd4ac5e0f5994c822c6022cca4
owhenrique <me.pauloalmeida@gmail.com>
2024-05-05 23:33:38 -0300

    feat: add student workout

---
+++ src/mocks/aluno.ts
@@ -0,0 +1,12 @@
+import { Aluno } from "@/interfaces/types";
+
+const aluno: Aluno = {
+    foto: "https://raw.githubusercontent.com/owhenrique/workout-plan-rm/main/public/avatar.jpeg",
+    sigla: "RM",
+    nome: "Karla Renata",
+    idade: "42",
+    peso: "74",
+    objetivo: "Emagrecimento"
+}
+
+export default aluno;
---
+++ src/mocks/treinoB.ts
@@ -0,0 +1,18 @@
+import { Treino } from "@/interfaces/types";
+
+const treinoB: Treino = {
+    categoria: "Pernas",
+    exercicios: [
+      { imagem: "https://fitnessprogramer.com/wp-content/uploads/2021/08/Lever-Horizontal-Leg-Press.gif", nome: "Leg press", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://media.tenor.com/bqKtsSuqilQAAAAM/gym.gif", nome: "Cadeira extensora", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://media.tenor.com/ZElx6PviDq0AAAAM/gym.gif", nome: "Cadeira flexora", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2017/11/smith-full-squat.gif", nome: "Agachamento Smith", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2023/10/Dumbbell-Bulgarian-Split-Squat.gif", nome: "Agachamento Búlgaro", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/11/coice-no-cabo.gif", nome: "Coice na polia", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.blog.treinoemalta.com.br/wp-content/uploads/2023/07/sentado.gif", nome: "Panturilha sentado", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/corrida-na-esteira-inclinada-1.gif", nome: "Esteira", duracao: 15 }
+    ]
+  };
+  
+  export default treinoB;
+  
